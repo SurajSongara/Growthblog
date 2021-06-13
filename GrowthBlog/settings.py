c9262c576b824ae7fb4749f79fe15d6dc05b5066
SurajSongara <SurajSongara>
2021-06-13 09:22:52 +0530

    Setup added

--- GrowthBlog/settings.py
+++ GrowthBlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '-k_4$+i#=en@9ar96$d_w=udpnfce%ve%q!4g1tn2()h=ech4n'
 
+
+SECRET_KEY=os.environ['SECRET_KEY']
+
+
+#
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG=False
+
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[BASE_DIR/'static',]
+STATIC_ROOT=BASE_DIR/ 'static'
+# STATICFILES_DIRS=[BASE_DIR/'static',]
 
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
